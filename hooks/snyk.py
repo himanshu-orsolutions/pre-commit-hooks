b482ee0bde06658097d6e7860c35db9fd5a896d3
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2022-03-04 13:27:27 +0530

    refactor hooks

--- hooks/snyk.py
+++ hooks/snyk.py
@@ -1,17 +1,9 @@
-from subprocess import check_output, CalledProcessError, STDOUT
-import sys
-
-def execute_command(command):
-    try:
-        check_output(command, stderr=STDOUT, shell=True)
-        success = True 
-    except CalledProcessError as e:
-        success = False
-    return success
+import subprocess
 
 def main():
-    success = execute_command('snyk test --severity-threshold=high')
-    return 0 if success else 1
+    status, output = subprocess.getstatusoutput('snyk test --severity-threshold=high')
+    print(output)
+    return 0 if status==0 else 1
 
 if __name__ == '__main__':
-    sys.exit(main())
+    raise SystemExit(main())
--- hooks/spotbugs.py
+++ hooks/spotbugs.py
@@ -1,14 +1,9 @@
 import subprocess
 
 def main():
-    result = subprocess.run('mvnw spotbugs:check', capture_output=True, shell=True)
-
-    if result.returncode == 0:
-        print(result.stdout)
-        return 0
-    else:
-        print(result.stderr)
-        return 1
+    status, output = subprocess.getstatusoutput('mvnw spotbugs:check')
+    print(output)
+    return 0 if status==0 else 1
 
 if __name__ == '__main__':
     raise SystemExit(main())
